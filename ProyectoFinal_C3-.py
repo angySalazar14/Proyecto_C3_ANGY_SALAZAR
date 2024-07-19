2bbee75d4aa3d87904651905b3a66a356dd9910b
Angy Lizeth Salazar Morales <alsalazarm@ut.edu.co>
2024-07-18 21:32:28 -0500

    en el testing con ProyectoFinal_C3-.py

--- ProyectoFinal_C3-.py
+++ ProyectoFinal_C3-.py
@@ -12,7 +12,7 @@ class Empleado():
     
     def __str__(self):
       return f" Empleado: {self.obtener_nombre_completo}, edad: {self.edad}, Salario: {self.sal},
-      DNI: {self.dni}, Fecha de vinculacion: " {self.fechav}
+      DNI: {self.dni}, Fecha de vinculacion:  {self.fechav}"
         
 empleado1 = Empleado('Andres', 'Prada', 30, 2000000, 1033, "20-03-2023")
 #print (f"Nombre del empleado: ", empleado1.getNombre())
@@ -22,6 +22,15 @@ print(empleado1)
 
 #empleado1.setEdad(57)
 #print(empleado1.getEdad())
-#print(empleado1.getFecha())    
+#print(empleado1.getFecha())   
+
+class Jefe(Empleado):
+    def __init__(self, nom, apell, edad, sal, dni, fechav):
+        super().__init__(nom, apell, edad, sal, dni, fechav)
+        self.empleAcargo = []  
+         
+    def agregar_empleado(self, empleado):
+        self.empleados.append(empleado) 
+             
 
 
--- ProyectoFinal_C3.py
+++ ProyectoFinal_C3.py
@@ -6,6 +6,10 @@ class Empleado():
       self.__edad = edad
       self._dni = dni
       self.__fechavin = fechav
+    
+    def obtener_Nombre_Completo(self):
+        return f"{self.nombre} {self.apellido}"  
+      
       
     def getNombre(self):
       return self.__nombre
@@ -18,40 +22,46 @@ class Empleado():
     
     def setEdad(self, edad):
       self.__edad = edad
-           
-    def obtener_Nombre_Completo(self):
-        return f"{self.nombre} {self.apellido}"
+
+empleado1 = Empleado('Andres', 'Prada', 30, 2000000, 1033, 20-03-2023)
+#print (f"Nombre del empleado: ", empleado1.getNombre())
+#print (f"Edad del empleado: ", empleado1.getEdad())
+print(empleado1)
+
+empleado2 = Empleado('Andres', 'Prada', 30, 2000000, 1033, 20-03-2023)
+print(empleado2)
+empleado3 = Empleado('Andres', 'Prada', 30, 2000000, 1033, 20-03-2023)
+print(empleado3)
+empleado4 = Empleado('Andres', 'Prada', 30, 2000000, 1033, 20-03-2023)
+print(empleado4)
+
+          
     
 class Jefe(Empleado):
     def __init__(self, nom, apell, edad, sal, dni, fechav):
         super().__init__(nom, apell, edad, sal, dni, fechav)
-        self.empleAcargo = empleAC   
+        self.empleAcargo = []  
+         
+    def agregar_empleado(self, empleado):
+        self.empleados.append(empleado)     
         
+    def obtener_cantidad_empleados(self):
+      return len(self.empleados)
+    
+    def obtener_empleados(self):
+      return self.empleados
+            
 class Area():
     def __init__(self, nom, descripcion ):
        self.nombre = nom
        self.descripcion = descripcion
        self.empleados = []
        
-    def agregar_empleado(self, empleado):
-        self.empleados.append(empleado) 
-    
-    def obtener_cantidad_empleados(self):
-      return len(self.empleados)
-    
-    def obtener_empleados(self):
-      return self.empleados
-    
+  
     def __str__(self):
       return f" Area: {self.nombre}, Descripcion: {self.descripcion}, Cantidad de empleados: {len(self.empleados)}"
         
-empleado1 = Empleado('Andres', 'Prada', 30, 2000000, 1033, 20-03-2023)
-print (f"Nombre del empleado: ", empleado1.getNombre())
-print (f"Edad del empleado: ", empleado1.getEdad())
 
-empleado1.setEdad(57)
-print(empleado1.getEdad())
-print(empleado1.getFecha())
 
 
 
